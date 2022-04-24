7850500ac02eeb32b297d0230cf2c6b78f5c2f80
ghufran adel <gho0ofran.adel14@gmail.com>
2022-04-24 01:43:44 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let cards = ["Charlie", "Samip", "Ali"];
+let arr= [];
+a
+function writeCards(cards) {  
+    for( let i=0 ; i<cards.length ; i++){
+     let birthday = arr.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+      console.log (birthday);
+        }
+        return arr;
+    }
+    
+    function countDown(num) {
+        let CountDown =num ;
+        while (CountDown >= 0) {
+            console.log(CountDown--);
+          }
+          return num ;
+    }
+    
+
